c8dbee423cb29e018aaea47fd737fb8a57301827
NISARG CHAUDHARY <nisargkumar2005@gmail.com>
2025-06-28 15:42:31 +0530

    bugs fixed first if command is empty then no command will execute and try to take new command second now you can open specific profile for specific link and some random bugs fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser as wb
 import pyttsx3
+import subprocess
 import requests
 import os
 
@@ -17,6 +18,14 @@ engine = pyttsx3.init()
 duration = 5
 samplerate = 16000
 
+brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+
+profile1 = "Profile 1" # --> gaming
+profile2 = "Profile 2" # --> nisargkumar (profectional)
+profile3 = "profile 3" # --> college
+profile4 = "Default" # --> nisarg
+
+
 def speak(text):
 
     engine.say(text)
@@ -25,12 +34,15 @@ def speak(text):
 def process_command(c):
     if "open google" in c.lower():
         wb.open("https://www.google.com")
-    elif "open youtube" in c.lower():
+
+    elif "open youtube" in c.lower() or "open you tube" in c.lower():
         wb.open("https://www.youtube.com")
-    elif "open chatgpt" or "open chat gpt" or "open chat g p t" in c.lower():
+
+    elif "open chatgpt" in c.lower() or "open chat gpt" in c.lower():
         wb.open("https://www.chatgpt.com")
-    elif "open linkedin" or "open linked in" or "open link in" in c.lower():
-        wb.open("https://www.linkedin.com")
+
+    elif "open linkedin" in c.lower() or "open linked in" in c.lower() or "open link in" in c.lower():
+        subprocess.Popen([brave_path, f"--profile-directory={profile2}", "https://www.linkedin.com"])
     else:
         print("unknown command")
 
@@ -55,7 +67,9 @@ if(__name__ == "__main__"):
         result = model.transcribe(audio_path)
         print(result["text"])
 
-        process_command(result["text"])
+        if(result["text"] != ""):
+            process_command(result["text"])
+            break
         
         # except Exception as e:
         #     print("Error; {0}".format(e))
